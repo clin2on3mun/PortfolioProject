2131ea93988d6eaedf5f177f64269d83017ae5f5
Munana Clinton <munanaclinton@gmail.com>
2023-04-13 09:14:03 +0200

    Add changes to files

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 const hamburgMenu = document.querySelector('section.menu-bar');
 const menuList = document.querySelector('.nav-links');
 const closeIcon = document.querySelector('.close');
-const workSection= document.querySelector('#work-section');
-const cardSection = document.createElement('section')
-const backdrop= document.querySelector('#backdrop');
-const modals= document.querySelector('.modal')
-cardSection.id="card-section";
+const workSection = document.querySelector('#work-section');
+const cardSection = document.createElement('section');
+const backdrop = document.querySelector('#backdrop');
+const modals = document.querySelector('.modal');
+cardSection.id = 'card-section';
 
-const modalPopUp=[{
-  modalTitle: ['Multi Post Stories','keep track of hundred of components website'],
-  listOfTechnologies:['HTML','Bootstrap','Ruby on Rails'],
-  imageModal:'images/feature_image.png',
-  modalDescription:`Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+const modalPopUp = [{
+  modalTitle: ['Multi Post Stories', 'keep track of hundred of components website'],
+  listOfTechnologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+  imageModal: 'images/feature_image.png',
+  modalDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
   Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
    printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five 
    centuries, but also the leap into electronic typesetting, remaining essent`,
-  SeeLive:'See live',
-  seeSource:'See source',
+  SeeLive: 'See live',
+  seeSource: 'See source',
 
-}]
+}];
 
 const recentWorkContent = [{
   recentWorkTitle: 'My Recent Works',
-  imageSource: "images/Img-Placeholder.png",
+  imageSource: 'images/Img-Placeholder.png',
   ProjectDescriptionTitle: 'Multi-Post Stories',
   projectDescription: `A daily selection of privately personalized reads;
   no accounts or sign-ups required. has been the industry's standard dummy
   text ever since the 1500s, when an unknown printer took a standard dummy text.`,
-  recentWorkButton:['CSS','HTML','Bootstrap','Ruby','See project']
-}]
+  recentWorkButton: ['CSS', 'HTML', 'Bootstrap', 'Ruby', 'See project'],
+}];
 
-const cardSectionContent =[{
+const cardSectionContent = [{
   cardTitles: 'Profesional Art Printing Data',
-  cardTitles1:['Data Dashboard Healthcare', 'Website Portfolio', 'Proffesional Art Printing Data More', ],
+  cardTitles1: ['Data Dashboard Healthcare', 'Website Portfolio', 'Proffesional Art Printing Data More'],
   cardDescription: `A daily selection of privately personalized reads;
   no accounts or sign-ups required. Has been the industry's standard.`,
-  cardButton:['HTML', 'Bootstrap', 'Ruby', 'See project'],
-}]
+  cardButton: ['HTML', 'Bootstrap', 'Ruby', 'See project'],
+}];
 
-recentWorkContent.forEach(e =>{
-  workSection.insertAdjacentHTML('afterbegin',`<section class="recent-work">
+recentWorkContent.forEach((e) => {
+  workSection.insertAdjacentHTML('afterbegin', `<section class="recent-work">
   <div class="title-recent-word"><h2 id="recent-work-title">${e.recentWorkTitle}</h2>
   <hr></div>
   <div class="multipost">
@@ -61,18 +61,18 @@ recentWorkContent.forEach(e =>{
   </section>
   </div>
   </div>  
-</section>`)
-})
+</section>`);
+});
 
-const recentWork =document.querySelector('.recent-work');
+const recentWork = document.querySelector('.recent-work');
 
-recentWork.insertAdjacentElement('afterend',cardSection)
+recentWork.insertAdjacentElement('afterend', cardSection);
 
-const cardSectionOutput=() =>{
-let result;
-for(let i = 1 ; i <= 6 ; i++){
- result= cardSectionContent.forEach(e => {
-    cardSection.insertAdjacentHTML('afterbegin', `<section class="post">
+const cardSectionOutput = () => {
+  let result;
+  for (let i = 1; i <= 6; i += 1) {
+    result = cardSectionContent.forEach((e) => {
+      cardSection.insertAdjacentHTML('afterbegin', `<section class="post">
     <section class="card-contents">
         <h3 class="card-main-title">${e.cardTitles}</h3>
         <p class="card-description">${e.cardDescription}</p>
@@ -85,38 +85,36 @@ for(let i = 1 ; i <= 6 ; i++){
     </section>    
     </section> 
     <button type="button" class="card-big-btn" >${e.cardButton[3]}</button>    
-</section>`)
- const h3Title= document.querySelector('.card-main-title')
-if(i == 1 || i== 4){
-  h3Title.insertAdjacentHTML('afterend',`<h3 class="card-main-title1">${e.cardTitles1[1]}</h3>`)
-} 
-if(i==2 || i==5){
-  h3Title.insertAdjacentHTML('afterend',`<h3 class="card-main-title1">${e.cardTitles1[0]}</h3>`)
-} 
-if(i === 3){
-  h3Title.insertAdjacentHTML('afterend',`<h3 class="card-main-title1">${e.cardTitles1[2]}</h3>`)
-}
+</section>`);
+      const h3Title = document.querySelector('.card-main-title');
+      if (i === 1 || i === 4) {
+        h3Title.insertAdjacentHTML('afterend', `<h3 class="card-main-title1">${e.cardTitles1[1]}</h3>`);
+      }
+      if (i === 2 || i === 5) {
+        h3Title.insertAdjacentHTML('afterend', `<h3 class="card-main-title1">${e.cardTitles1[0]}</h3>`);
+      }
+      if (i === 3) {
+        h3Title.insertAdjacentHTML('afterend', `<h3 class="card-main-title1">${e.cardTitles1[2]}</h3>`);
+      }
+    });
+  }
+  return result;
+};
 
-})
-   
-}
-return result;
-}
-
-const modalContent=() => {
-  let result;
-  result = modalPopUp.forEach(e => {
-    modals.insertAdjacentHTML('afterbegin',`
+const modalContent = () => {
+  const result = modalPopUp.forEach((e) => {
+    modals.insertAdjacentHTML('afterbegin', `
     <img src="images/Icon.png" class="close-icon">
     <h3 class="modalTitle">${e.modalTitle[0]}</h3>
     <h3 class="modaltitle">${e.modalTitle[1]} </h3> 
     <div class="modal-content">
-    <ul class="buttons recent-work-buttons">
+    <ul class="buttons grid-button recent-work-buttons">
     <li><button type="button"  class="btn-work">CSS</button></li>
     <li><button type="button" class="btn-work">HTML</button></li>
     <li><button type="button" class="btn-work">Bootstrap</button></li>
     </ul>
-        <img src="${e.imageModal}" alt = "Modal image">
+        <img src="${e.imageModal}" alt = "Modal image" class="feature_img">
+        <div class="grid-modal">
         <p class="paragraph">${e.modalDescription}</p>
         <p class="modal-descriptions">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
@@ -126,29 +124,36 @@ const modalContent=() => {
         <ul class="modal-button-pop">
             <li><button type="button" class="view-buttons button-live"><span>${e.SeeLive}</span><img src="images/live.png" class="live" alt="live"></button></li>
             <li><button type="button" class="view-buttons button-source"><span>${e.seeSource}</span><img src="images/Group.png" class="source" alt="source"></button></li>
-        </ul>  
-       </div>  
-`)
-  })
+        </ul> 
+        </div> 
+       </div> 
+       
+`);
+  });
   return result;
-}
+};
 
-modalContent()
-cardSectionOutput()
+modalContent();
+cardSectionOutput();
 
-const buttonRecentWork= document.querySelector('.modal-button')
+const buttonRecentWork = document.querySelector('.modal-button');
 const buttonCard = document.querySelectorAll('.card-big-btn');
 
-buttonRecentWork.addEventListener('click', () =>{
+buttonRecentWork.addEventListener('click', () => {
   backdrop.classList.add('active');
-  modals.classList.add('active')
-})
-let closebutton =document.querySelector('img.close-icon')
-closebutton.addEventListener('click', () =>{
+  modals.classList.add('active');
+  // modals.style.overflow="scroll";
+});
+buttonCard.forEach((e) => e.addEventListener('click', () => {
+  backdrop.classList.add('active');
+  modals.classList.add('active');
+}));
+const closebutton = document.querySelector('img.close-icon');
+closebutton.addEventListener('click', () => {
   backdrop.classList.remove('active');
   modals.classList.remove('active');
-  console.log('closed')
-})
+  console.log('closed');
+});
 
 hamburgMenu.addEventListener('click', () => {
   menuList.classList.toggle('visible');
