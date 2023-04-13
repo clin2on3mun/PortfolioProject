ce953c7fe5c9b4d591b56e8ddf69438664821cac
Munana Clinton <munanaclinton@gmail.com>
2023-04-13 21:22:41 +0200

    Add changes to css and script files

--- script.js
+++ script.js
@@ -3,22 +3,7 @@ const menuList = document.querySelector('.nav-links');
 const closeIcon = document.querySelector('.close');
 const workSection = document.querySelector('#work-section');
 const cardSection = document.createElement('section');
-const backdrop = document.querySelector('#backdrop');
-const modals = document.querySelector('.modal');
 cardSection.id = 'card-section';
-
-const modalPopUp = [{
-  modalTitle: ['Multi Post Stories', 'keep track of hundred of components website'],
-  listOfTechnologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
-  imageModal: 'images/feature_image.png',
-  modalDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-   printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five 
-   centuries, but also the leap into electronic typesetting, remaining essent`,
-  SeeLive: 'See live',
-  seeSource: 'See source',
-
-}];
 
 const recentWorkContent = [{
   recentWorkTitle: 'My Recent Works',
@@ -28,15 +13,91 @@ const recentWorkContent = [{
   no accounts or sign-ups required. has been the industry's standard dummy
   text ever since the 1500s, when an unknown printer took a standard dummy text.`,
   recentWorkButton: ['CSS', 'HTML', 'Bootstrap', 'Ruby', 'See project'],
+  SeeLive: '',
+  seeSource: '',
+
 }];
 
 const cardSectionContent = [{
-  cardTitles: 'Profesional Art Printing Data',
-  cardTitles1: ['Data Dashboard Healthcare', 'Website Portfolio', 'Proffesional Art Printing Data More'],
+  id: '1',
+  cardTitles: 'Website Portfolio',
   cardDescription: `A daily selection of privately personalized reads;
   no accounts or sign-ups required. Has been the industry's standard.`,
+  modalDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown okay sir
+  `,
   cardButton: ['HTML', 'Bootstrap', 'Ruby', 'See project'],
-}];
+  SeeLive: '',
+  seeSource: '',
+  imageModal: 'images/feature_image.png',
+},
+{
+  id: '2',
+  cardTitles: 'Profesional Art Printing Data',
+  cardDescription: `A daily selection of privately personalized reads;
+  no accounts or sign-ups required. Has been the industry's standard.`,
+  modalDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  Lorem Ipsum has been `,
+  cardButton: ['HTML', 'Bootstrap', 'Ruby', 'See project'],
+  SeeLive: '',
+  seeSource: '',
+  imageModal: 'images/feature_image.png',
+},
+
+{
+  id: '3',
+  cardTitles: 'Profesional Art Printing Data',
+  cardDescription: `A daily selection of privately personalized reads;
+    no accounts or sign-ups required. Has been the industry's standard.`,
+  modalDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown okay sir`,
+  cardButton: ['HTML', 'Bootstrap', 'Ruby', 'See project'],
+  SeeLive: '',
+  seeSource: '',
+  imageModal: 'images/feature_image.png',
+},
+{
+  id: '4',
+  cardTitles: 'Profesional Art Printing Data',
+  cardDescription: `A daily selection of privately personalized reads;
+      no accounts or sign-ups required. Has been the industry's standard.`,
+  modalDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+      printer took a galley of type and scrambled it to make a type`,
+  cardButton: ['HTML', 'Bootstrap', 'Ruby', 'See project'],
+  SeeLive: '',
+  seeSource: '',
+  imageModal: 'images/feature_image.png',
+},
+{
+  id: '5',
+  cardTitles: 'Profesional Art Printing Data',
+  cardDescription: `A daily selection of privately personalized reads;
+        no accounts or sign-ups required. Has been the industry's standard.`,
+  modalDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting when an unknown
+       printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five 
+       centuries, but also the leap into electronic typesetting, remaining essent`,
+  cardButton: ['HTML', 'Bootstrap', 'Ruby', 'See project'],
+  SeeLive: '',
+  seeSource: '',
+  imageModal: 'images/feature_image.png',
+},
+{
+  id: '6',
+  cardTitles: 'Profesional Art Printing Data',
+  cardDescription: `A daily selection of privately personalized reads;
+          no accounts or sign-ups required. Has been the industry's standard.`,
+  modalDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+    `,
+  cardButton: ['HTML', 'Bootstrap', 'CSS', 'See project'],
+  SeeLive: '',
+  seeSource: '',
+  imageModal: 'images/feature_image.png',
+},
+
+];
 
 recentWorkContent.forEach((e) => {
   workSection.insertAdjacentHTML('afterbegin', `<section class="recent-work">
@@ -60,8 +121,27 @@ recentWorkContent.forEach((e) => {
       
   </section>
   </div>
-  </div>  
-</section>`);
+  </div>
+  <div id="backdrop"></div>
+            <div class="modal card">
+            <img src="images/Icon.png" class="close-icon">
+            <h3 class="modalTitle">${e.recentWorkTitle}</h3>
+            <div class="modal-content">
+            <ul class="buttons grid-button recent-work-buttons">
+            <li><button type="button"  class="btn-work">${e.recentWorkButton[0]}</button></li>
+            <li><button type="button" class="btn-work">${e.recentWorkButton[1]}</button></li>
+            <li><button type="button" class="btn-work">${e.recentWorkButton[3]}</button></li></ul>
+            <img src="${e.imageSource}" alt = "project image" class="feature_img">
+            <div class="grid-modal">
+            <p class="paragraph">${e.projectDescription}</p>
+            <ul class="modal-button-pop">
+                <li><button type="button" class="view-buttons button-live"><a href="${e.SeeLive}" class="popUp-links"><span>see Live</span><img src="images/live.png" class="live" alt="live"></a></button></li>
+                <li><button type="button" class="view-buttons button-source"><a href="${e.seeSource}" class="popUp-links"><span> see Source </span><img src="images/Group.png" class="source" alt="source"></a></button></li>
+            </ul> 
+            </div> 
+           </div> 
+            </div>  
+  </section>`);
 });
 
 const recentWork = document.querySelector('.recent-work');
@@ -69,89 +149,85 @@ const recentWork = document.querySelector('.recent-work');
 recentWork.insertAdjacentElement('afterend', cardSection);
 
 const cardSectionOutput = () => {
-  let result;
-  for (let i = 1; i <= 6; i += 1) {
-    result = cardSectionContent.forEach((e) => {
-      cardSection.insertAdjacentHTML('afterbegin', `<section class="post">
+  cardSection.innerHTML = cardSectionContent.map((x) => {
+    const {
+      id, cardTitles, cardDescription, cardButton, imageModal, modalDescription, SeeLive, seeSource,
+    } = x;
+
+    return `<section class="post">
     <section class="card-contents">
-        <h3 class="card-main-title">${e.cardTitles}</h3>
-        <p class="card-description">${e.cardDescription}</p>
+        <h3 class="card-main-title">${cardTitles}</h3>
+        <p class="card-description">${cardDescription}</p>
         <section class="card-buttons">
           <ul class="buttons">  
-           <li><button type="button" class="btn-card">${e.cardButton[0]}</button></li>
-           <li><button type="button" class="btn-card">${e.cardButton[1]}</button></li>
-           <li><button type="button" class="btn-card">${e.cardButton[2]}</button></li>
+           <li><button type="button" class="btn-card">${cardButton[0]}</button></li>
+           <li><button type="button" class="btn-card">${cardButton[1]}</button></li>
+           <li><button type="button" class="btn-card">${cardButton[2]}</button></li>
         </ul>
     </section>    
     </section> 
-    <button type="button" class="card-big-btn" >${e.cardButton[3]}</button>    
-</section>`);
-      const h3Title = document.querySelector('.card-main-title');
-      if (i === 1 || i === 4) {
-        h3Title.insertAdjacentHTML('afterend', `<h3 class="card-main-title1">${e.cardTitles1[1]}</h3>`);
-      }
-      if (i === 2 || i === 5) {
-        h3Title.insertAdjacentHTML('afterend', `<h3 class="card-main-title1">${e.cardTitles1[0]}</h3>`);
-      }
-      if (i === 3) {
-        h3Title.insertAdjacentHTML('afterend', `<h3 class="card-main-title1">${e.cardTitles1[2]}</h3>`);
-      }
-    });
-  }
-  return result;
+    <button type="button" class="card-big-btn" data-modal-target="#modal${id}">${cardButton[3]}</button>  
+    <div id="backdrop"></div>
+            <div class=" card modal-card-section" id="modal${id}">
+            <button type="button" data-modal-close-button> <img src="images/Icon.png" class="close-icon"></button>
+            <h3 class="modalTitle">${cardTitles}</h3>
+            <div class="modal-content">
+            <ul class="buttons grid-button recent-work-buttons">
+            <li><button type="button"  class="btn-work">${cardButton[0]}</button></li>
+            <li><button type="button" class="btn-work">${cardButton[1]}</button></li>
+            <li><button type="button" class="btn-work">${cardButton[2]}</button></li></ul>
+            <img src="${imageModal}" alt = "project image" class="feature_img">
+            <div class="grid-modal">
+            <p class="paragraph">${modalDescription}</p>
+            <ul class="modal-button-pop">
+                <li><button type="button" class="view-buttons button-live"><a href="${SeeLive}" class="popUp-links"><span>see Live</span><img src="images/live.png" class="live" alt="live"></a></button></li>
+                <li><button type="button" class="view-buttons button-source"><a href="${seeSource}" class="popUp-links"><span> see Source </span><img src="images/Group.png" class="source" alt="source"></a></button></li>
+            </ul> 
+            </div> 
+           </div> 
+            </div>  
+</section>`;
+  }).join('');
 };
 
-const modalContent = () => {
-  const result = modalPopUp.forEach((e) => {
-    modals.insertAdjacentHTML('afterbegin', `
-    <img src="images/Icon.png" class="close-icon">
-    <h3 class="modalTitle">${e.modalTitle[0]}</h3>
-    <h3 class="modaltitle">${e.modalTitle[1]} </h3> 
-    <div class="modal-content">
-    <ul class="buttons grid-button recent-work-buttons">
-    <li><button type="button"  class="btn-work">CSS</button></li>
-    <li><button type="button" class="btn-work">HTML</button></li>
-    <li><button type="button" class="btn-work">Bootstrap</button></li>
-    </ul>
-        <img src="${e.imageModal}" alt = "Modal image" class="feature_img">
-        <div class="grid-modal">
-        <p class="paragraph">${e.modalDescription}</p>
-        <p class="modal-descriptions">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it 1960s.
-
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.</p>
-        <ul class="modal-button-pop">
-            <li><button type="button" class="view-buttons button-live"><a href="https://clin2on3mun.github.io/PortfolioProject/" class="popUp-links"><span>${e.SeeLive}</span><img src="images/live.png" class="live" alt="live"></a></button></li>
-            <li><button type="button" class="view-buttons button-source"><a href="https://clin2on3mun/PortfolioProject/" class="popUp-links"><span>${e.seeSource}</span><img src="images/Group.png" class="source" alt="source"></a></button></li>
-        </ul> 
-        </div> 
-       </div> 
-       
-`);
-  });
-  return result;
-};
-
-modalContent();
 cardSectionOutput();
 
 const buttonRecentWork = document.querySelector('.modal-button');
-const buttonCard = document.querySelectorAll('.card-big-btn');
-
+const buttonCard = document.querySelectorAll('[data-modal-target]');
+const backdrop = document.querySelector('#backdrop');
+const modals = document.querySelector('.modal');
+const closebutton = document.querySelector('img.close-icon');
+const closebuttonCard = document.querySelectorAll('[data-modal-close-button]');
 buttonRecentWork.addEventListener('click', () => {
   backdrop.classList.add('active');
   modals.classList.add('active');
-  // modals.style.overflow="scroll";
 });
-buttonCard.forEach((e) => e.addEventListener('click', () => {
-  backdrop.classList.add('active');
-  modals.classList.add('active');
-}));
-const closebutton = document.querySelector('img.close-icon');
 closebutton.addEventListener('click', () => {
   backdrop.classList.remove('active');
   modals.classList.remove('active');
+});
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  backdrop.classList.add('active');
+}
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  backdrop.classList.remove('active');
+}
+
+buttonCard.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+closebuttonCard.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal-card-section');
+    closeModal(modal);
+  });
 });
 
 hamburgMenu.addEventListener('click', () => {
